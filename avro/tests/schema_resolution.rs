1a31344ad8a228bca1b0edeed67668f2e4ce3474
Kit Danison-Fieldhouse <kit.fieldhouse@gmail.com>
2025-12-28 17:08:08 -0600

    add schema resolution test based off of schema name

--- avro/tests/schema_resolution.rs
+++ avro/tests/schema_resolution.rs
@@ -29,6 +29,33 @@ use apache_avro_test_helper::{
     init,
 };
 
+#[derive(Clone, Default)]
+struct TestBuffer(Rc<RefCell<Vec<u8>>>, usize);
+
+impl Write for TestBuffer {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.0.borrow_mut().write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+impl Read for TestBuffer {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let vec = self.0.borrow_mut();
+        let left_to_read = vec.len() - self.1;
+        let to_read = if buf.len() > left_to_read {left_to_read} else {buf.len()};
+        let slice = &vec[self.1..(self.1+to_read)];
+        slice.iter().enumerate().for_each(|(index, value)|{
+            buf[index] = *value;
+        });
+        self.1 = self.1 + to_read;
+        Ok(to_read)
+    }
+}
+
 #[test]
 fn test_long_to_int() -> TestResult {
     init();
@@ -61,32 +88,63 @@ fn test_long_to_int() -> TestResult {
     )
     ]);
 
-    #[derive(Clone, Default)]
-    struct TestBuffer(Rc<RefCell<Vec<u8>>>, usize);
+    let buffer = TestBuffer(Rc::new(RefCell::new(Vec::new())), 0);
 
-    impl Write for TestBuffer {
-        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-            self.0.borrow_mut().write(buf)
-        }
+    let writer_schema = Schema::parse_str(writer_schema)?;
 
-        fn flush(&mut self) -> std::io::Result<()> {
-            Ok(())
+    let mut writer = Writer::new(&writer_schema, buffer.clone())?;
+
+    let _ = writer.append(value);
+    let _ = writer.flush();
+
+    let reader_schema = Schema::parse_str(reader_schema)?;
+    let mut reader = Reader::with_schema(&reader_schema,buffer)?;
+
+    let read_value = reader.next();
+
+    match read_value {
+        Some(v) => {
+            panic!("Reader and writer schema are incompatible, should not be able to deserialize. Desearlized value: {:?}", v)
         }
+        None => {}
     }
+    Ok(())
+}
 
-    impl Read for TestBuffer {
-        fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-            let vec = self.0.borrow_mut();
-            let left_to_read = vec.len() - self.1;
-            let to_read = if buf.len() > left_to_read {left_to_read} else {buf.len()};
-            let slice = &vec[self.1..(self.1+to_read)];
-            slice.iter().enumerate().for_each(|(index, value)|{
-                buf[index] = *value;
-            });
-            self.1 = self.1 + to_read;
-            Ok(to_read)
+
+#[test]
+fn test_record_resolution() -> TestResult {
+    // this test should fail per the specifications requirements that
+    // record - record resolution checks strict equality of record names
+    init();
+    let writer_schema = r#"{
+        "type": "record",
+        "name": "schemaA",
+        "fields": [
+        {
+            "name": "aNumber",
+            "type": "int"
         }
-    }
+        ]
+    }"#;
+
+    let reader_schema = r#"{
+        "type": "record",
+        "name": "schemaB",
+        "fields": [
+        {
+            "name": "aNumber",
+            "type": "int"
+        }
+        ]
+    }"#;
+
+    // value that was written with writer_schema
+    let value = Value::Record(vec![(
+            "aNumber".into(),
+            Value::Int(2000)
+    )
+    ]);
 
     let buffer = TestBuffer(Rc::new(RefCell::new(Vec::new())), 0);
 
